881ebd733b7ad4b1ce4fd6c5fe37c341931f4beb
Mzhdi <elmehdizeoruk@gmail.com>
2023-09-07 20:26:22 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/stock/<symbol>', methods=['GET'])
+def get_stock_data(symbol):
+    try:
+        # Fetch real-time stock data using yfinance
+        stock = yf.Ticker(symbol)
+        print(stock)
+        data = stock.history(period="1d")
+        print(data)
+        if not data.empty:
+            latest_data = data.iloc[-1].to_dict()
+            return jsonify(latest_data)
+        else:
+            return jsonify({'error': 'Unable to fetch data'}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
