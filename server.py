ee65466d4347818ebaa7a989d9e683f8017cfb48
mehdi bahou <bahoubuisness@gmail.com>
2023-09-10 13:09:25 +0100

    machine learning model integration

--- server.py
+++ server.py
@@ -1,24 +1,41 @@
 from flask import Flask, request, jsonify
-import yfinance as yf
+import joblib
 
 app = Flask(__name__)
 
-@app.route('/stock/<symbol>', methods=['GET'])
-def get_stock_data(symbol):
+# Load the saved model
+model_filename = 'stock_prediction_model.pkl'
+model = joblib.load(model_filename)
+
+
+def predict_stock_trade_decision(model, today_features):
+    # Predict whether it's a good day to open or close based on today's features
+    prediction = model.predict([today_features])
+
+    # Interpret the prediction
+    if prediction[0] == 1:
+        return "Open"  # Good day to open a position
+    else:
+        return "Close"  # Good day to close a position
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
     try:
-        # Fetch real-time stock data using yfinance
-        stock = yf.Ticker(symbol)
-        print(stock)
-        data = stock.history(period="1d")
-        print(data)
-        if not data.empty:
-            latest_data = data.iloc[-1].to_dict()
-            return jsonify(latest_data)
-        else:
-            return jsonify({'error': 'Unable to fetch data'}), 500
+        # Get data from the POST request
+        data = request.json
+        # Assuming the JSON request contains 'features'
+        today_features = data['features']
+
+        # Make a prediction using the loaded model
+        prediction = predict_stock_trade_decision(model, today_features)
+
+        # Return the prediction as JSON response
+        response = {'prediction': prediction}
+        return jsonify(response)
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
